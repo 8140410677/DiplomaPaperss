f3c80742170e55d4106f1fe43888c703c68ae30c
Priyanshu <priyanshuparjaparti@gmail.com>
2025-08-06 19:59:44 +0530

    splashScreen

---
+++ app/src/main/java/mrkinfotech/priyanshu/diplomapaperss/ui/basic/SplashActivity.kt
@@ -0,0 +1,19 @@
+package mrkinfotech.priyanshu.diplomapaperss.ui.basic
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import mrkinfotech.priyanshu.diplomapaperss.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash)
+
+
+
+    }
+}
