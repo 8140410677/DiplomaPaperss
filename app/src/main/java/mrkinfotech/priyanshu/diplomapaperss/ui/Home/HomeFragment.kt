ede5dbb7a0e0c35cae16e7da1907f2ae97922ead
Priyanshu <priyanshuparjaparti@gmail.com>
2025-08-14 17:50:45 +0530

    Splash Activity

--- app/src/main/java/mrkinfotech/priyanshu/diplomapaperss/ui/Home/HomeFragment.kt
+++ app/src/main/java/mrkinfotech/priyanshu/diplomapaperss/ui/Home/HomeFragment.kt
@@ -21,7 +21,6 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
